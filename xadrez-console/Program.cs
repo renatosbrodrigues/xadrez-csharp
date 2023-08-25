72fa900552ec472ebd78b1dc21a251e2ab006e35
renatosbrodrigues <renatodesouza2607@gmail.com>
2023-08-25 10:29:15 -0300

    tabuleiro criado

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -1,17 +1,14 @@
 ﻿using System;
-using Tabuleiro;
+using tabuleiro;
 
 namespace xadrez_console
 {
     class Program
     {
         static void Main(string[] args) {
+            Tabuleiro tab = new Tabuleiro(8, 8);
 
-            Posicao P;
-
-            P = new Posicao(3, 4);
-
-            Console.WriteLine(P);
+            Console.ReadLine();
 
         }
     }
